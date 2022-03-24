77e758295bf63854cba221a8990013905b958bf7
SatomiRen <Kozhuhovfilipp@yandex.ru>
2022-03-24 12:48:08 +0300

    Commit 6

---
+++ Individual/F/i2__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["i2_info", "i2_display", "i2_select"]
---
+++ Individual/i2_main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from F import i2_display, i2_info, i2_select
+
+
+if __name__ == '__main__':
+    flights = []
+    while True:
+        command = input(">>> ").lower()
+        if command == 'exit':
+            break
+
+        elif command == 'add':
+            flight = i2_info.get_flight()
+            flights.append(flight)
+            if len(flights) > 1:
+                flights.sort(
+                    key=lambda item:
+                    item.get('flight_destination', ''))
+
+        elif command == 'list':
+            i2_display.display_flights(flights)
+
+        elif command.startswith('select '):
+            parts = command.split(' ', maxsplit=1)
+            airplane_type = (parts[1].capitalize())
+            print(f"Для типа самолета {airplane_type}:")
+            selected = i2_select.select_flights(flights, airplane_type)
+            i2_display.display_flights(selected)
+
+        elif command == 'help':
+            # Вывести справку о работе с программой.
+            print("Список команд:\n")
+            print("add - добавить рейс;")
+            print("list - вывести список всех рейсов;")
+            print(
+                "select <тип самолета> - запросить рейсы указанного типа "
+                "самолета;")
+            print("help - отобразить справку;")
+            print("exit - завершить работу с программой.")
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
